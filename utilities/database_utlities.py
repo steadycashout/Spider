a446a3c4d4ea9bfe5f6e93b728e69a23de708b94
Abraham <soulateboy@gmail.com>
2024-11-19 13:38:56 +0300

    initial commit

---
+++ utilities/database_utlities.py
@@ -0,0 +1,6 @@
+def create_database(database_path: str):
+    #TODO: generate the database
+
+def save_word_to_database(database_path: str, word_list: str):
+    #TODO: save the words to the database
+    pass
