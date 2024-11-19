01c9315fc0b954b9a4b7512b8f8dfab8cb99f33f
Abraham <soulateboy@gmail.com>
2024-11-19 13:45:58 +0300

    initial commit

--- utilities/database_utlities.py
+++ utilities/database_utlities.py
@@ -1,5 +1,6 @@
 def create_database(database_path: str):
     #TODO: generate the database
+    pass
 
 def save_word_to_database(database_path: str, word_list: str):
     #TODO: save the words to the database
--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,9 +1,10 @@
 def load_urls_from_file(file_path: str):
     #TODO: add the code needed to read the text file with the url in it
-pass
+    pass
 
 def load_page(url: str):
     #TODO: add the code that read the url
+    pass
 
 def scrape_page(page_contents: str):
     #TODO: analyse the text
